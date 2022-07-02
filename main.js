39ff2d17817f8f6a9f88c6d62ef43479e5e7b684
Veronica Orozco <93060985+VLOrozco@users.noreply.github.com>
2022-07-02 17:41:16 -0500

    Update main.js & style.scss
    adjustments to #navigation-menu
    
    added main.js file (needs toogle function for nav-menu)

---
+++ main.js
@@ -0,0 +1,11 @@
+// onclick
+// add menu-icon-fill
+// add navMenu.style.display = "block";
+
+// let navMenu = document.querySelector(".navMenu");
+// let isShow = true;
+
+// function toggleMenu() {
+//   isShow = !isShow;
+//   navMenu.classList.toggle("hide", isShow);
+// }
